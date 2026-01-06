4fa3a250bdf40544a39a5a07480e79f9784d633c
Prabashi Nuwani Jayaweera <nuwaniprabhashi2003@gmail.com>
2026-01-07 01:51:36 +0530

    files

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Loader from './components/Loader';
+const Dashboard = lazy(() => import("./pages/Dashboard"))
+const Products = lazy(() => import("./pages/Products"))
+const Transaction = lazy(() => import("./pages/Transaction"))
+const Customer = lazy(() => import("./pages/Customer"))
+
+
+const App = () => {
+  return (
+    <Router>
+      <Suspense fallback = {<Loader/>}>
+        <Routes>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/products" element={<Products />} />
+            <Route path="/admin/transaction" element={<Transaction />} />
+            <Route path="/admin/customer" element={<Customer />} />
+
+            {/* charts */}
+            {/* Apps */}
+          </Routes>
+      </Suspense>
+    </Router>
+  );
+};
+
+export default App;
